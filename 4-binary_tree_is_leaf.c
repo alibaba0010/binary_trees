3b117852fbf7317f464172276e53713bdf1c11df
alibaba0010 <zakariyahali100@gmail.com>
2023-10-30 12:15:34 +0100

    working on task five

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_leaf - check if a node is leaf
+ * @node: the node to check
+ *
+ * Return: 1 if node is leaf 1 otherwise 0
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if(node && (node->left || node->right))
+		return (0);
+	return (1);
+}	

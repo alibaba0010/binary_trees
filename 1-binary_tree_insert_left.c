757e160aa57e3699a970dc7949999607ae6c08b3
alibaba0010 <zakariyahali100@gmail.com>
2023-11-01 07:11:27 +0100

    debugging error

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,30 +1,36 @@
 #include "binary_trees.h"
+#include "binary_trees.h"
+
 /**
- * binary_tree_insert_left - add a node in the left of the parent
- * if it exists it move down one level and add the new node first
- * @parent: parent of the specified node
- * @value: value of the node to be inserted
- * Return: NULL if it fails 
- */
+ * binary_tree_insert_left - Adding new node on the left child
+ * @parent: the parent node
+ * @value: integer value to be insered in the new node
+ * Return: the new left child node
+ **/
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
 	if (parent == NULL)
-	{
 		return (NULL);
-	}
 
-	new_node = binary_tree_node(parent, value);
+	new_node = malloc(sizeof(binary_tree_t));
+
 	if (new_node == NULL)
-	{
 		return (NULL);
-	}
-	if (parent->left != NULL)
+
+	new_node->parent = parent;
+	new_node->n = value;
+	new_node->left = NULL;
+	new_node->right = NULL;
+
+	if (parent->left)
 	{
 		new_node->left = parent->left;
 		parent->left->parent = new_node;
 	}
 	parent->left = new_node;
+
 	return (new_node);
+
 }

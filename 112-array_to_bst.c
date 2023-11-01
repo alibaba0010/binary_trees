59db96dd5db726350360ca6cdbedd53fdd79d32a
alibaba0010 <zakariyahali100@gmail.com>
2023-11-01 18:50:02 +0100

    working on task 27

---
+++ 112-array_to_bst.c
@@ -0,0 +1,31 @@
+#include "binary_trees.h"
+/**
+ * array_to_bst - turns an array to a BST 
+ * @array: array to turns to BST 
+ * @size: size of array
+ * Return: BST from array
+ */
+bst_t *array_to_bst(int *array, size_t size)
+{
+	size_t len = 0;
+	bst_t *root;
+
+	root = NULL;
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	for (; len < size; len++)
+	{
+		if (len == 0)
+		{
+			bst_insert(&root, array[len]);
+		}
+		else
+		{
+			bst_insert(&root, array[len]);
+		}
+	}
+	return (root);
+}
+

9a08023be63b52669bd8674b24d0c5bd02f3fb3e
alibaba0010 <zakariyahali100@gmail.com>
2023-11-01 15:12:12 +0100

    debugging error

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,9 +11,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent != NULL)
-	{
-      new_node = binary_tree_node(parent, value);
+	if (parent == NULL)
+		return (NULL);	
+
+	new_node = binary_tree_node(parent, value);
 	if (new_node == NULL)
 		return (NULL);
 	
@@ -24,6 +25,4 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	}
 	parent->right = new_node;
 	return (new_node);
-	}
-	return (NULL);
 }

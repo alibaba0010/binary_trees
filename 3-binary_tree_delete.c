fe70a16a77db3e3d7b55ace810451b05e2387fe8
alibaba0010 <zakariyahali100@gmail.com>
2023-11-03 21:15:18 +0100

    debugging error

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -8,14 +8,18 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
+	if (tree == NULL)
 	{
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);
-		free(tree);
+		return;
 	}
 	else
 	{
-		return;		
+		if (tree != NULL)
+		{
+			binary_tree_delete(tree->left);
+			binary_tree_delete(tree->right);
+		}
+		free(tree);
 	}
+
 }

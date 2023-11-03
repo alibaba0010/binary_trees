b40a37d78b6c5578e86fef3fef657c5edabd6365
alibaba0010 <zakariyahali100@gmail.com>
2023-11-03 10:50:37 +0100

    debugging error

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -1,22 +1,17 @@
 #include "binary_trees.h"
+
 /**
- * binary_tree_delete - free an entire tree using recursion it free the node
- * when the node left and right are NULL
+ * binary_tree_delete - free an entire tree using recursion it frees the node
+ * when the node's left and right are NULL
  * @tree: tree to free
  * Return: Nothing if it fails
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	if (tree != NULL)
-	{
-      
-		binary_tree_delete(tree->left);
-		binary_tree_delete(tree->right);		
-		free(tree);
-	}
-	else
-	{
-		return;
-		
-	}
+    if (tree != NULL)
+    {
+        binary_tree_delete(tree->left);
+        binary_tree_delete(tree->right);
+        free(tree);
+    }
 }

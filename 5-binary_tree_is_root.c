826e2deb97c35e554e9248ccfac1d88669757e09
alibaba0010 <zakariyahali100@gmail.com>
2023-11-03 21:17:43 +0100

    debugging error

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -2,14 +2,16 @@
 
 /**
  * binary_tree_is_root - Check if a given node is root
- * @node: the node to check
- *
+ * @node: the node to check *
  * Return:node is root 1 otherwise 0
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if(node->parent)
+	if (node == NULL)
 		return (0);
-	return (1);
+	if (node->parent == NULL)
+		return (1);
+	else
+		return (0);
 }

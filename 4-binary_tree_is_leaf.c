81b42d2bfe8c630910ef7d4adbe7c4272469bc22
alibaba0010 <zakariyahali100@gmail.com>
2023-11-03 21:19:34 +0100

    debugging error

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -10,7 +10,9 @@ int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if(node && (node->left || node->right))
+
+	if (node->right == NULL && node->left == NULL)
+		return (1);
+	else
 		return (0);
-	return (1);
 }	

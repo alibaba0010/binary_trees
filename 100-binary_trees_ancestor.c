307f86a3715b9f92f86b269086cec38992d20f40
alibaba0010 <zakariyahali100@gmail.com>
2023-11-03 21:38:58 +0100

    debugging error

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -23,11 +23,13 @@ binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
 
 	first_node = first->parent;
 	second_node = second->parent;
-	if (first_node == NULL || first == second_node || (!first_node->parent && second_node))
+	if (first_node == NULL || first == second_node || 
+														(!first_node->parent && second_node))
 	{
 		return (binary_trees_ancestor(first, second_node));
 	}
-	else if (second_node == NULL || first_node == second || (!second_node->parent && first_node))
+	else if (second_node == NULL || first_node == second || 
+																(!second_node->parent && first_node))
 	{
 		return (binary_trees_ancestor(first_node, second));
 	}
